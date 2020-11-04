03c473a15b4303e1aab19b34e17f539a90e2a76c
Korol <zhuzhi@hellotalk.com>
2020-11-04 14:13:21 +0800

    s

--- fastlane/actions/jirastatus.rb
+++ fastlane/actions/jirastatus.rb
@@ -34,7 +34,7 @@ module Fastlane
         puts(people)
 
         require 'json'
-        text = File.read("~/.cache/fastlane/imported" + "/fastlane-ios/fastlane/userid.json")
+        text = File.read("~/.cache/fastlane/imported" + "/fastlane-ios.git/fastlane/userid.json")
         useidJson = JSON.parse(text)
 
         users = []
